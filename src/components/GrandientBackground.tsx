bd1712c57aa6c6062747e31e8a61debd7906cfe2
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-29 17:48:09 -0400

    LinearGradient has been incorporated on the grandeBackgroundComponent to test dif colors

--- src/components/GrandientBackground.tsx
+++ src/components/GrandientBackground.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 interface Props {
   children: JSX.Element | JSX.Element[]
@@ -7,7 +8,14 @@ interface Props {
 
 export const GrandientBackground = ({ children }: Props) => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#084F6A' }}>
+    <View style={{ flex: 1 }}>
+      <LinearGradient
+        colors={['#2D80D8', 'white', '#00458E']}
+        style={{ ...StyleSheet.absoluteFillObject }}
+        start={{ x: 0.1, y: 0.1 }}
+        end={{ x: 0.5, y: 0.5 }}
+      />
+
       {children}
     </View>
   )

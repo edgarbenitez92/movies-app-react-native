2971b8ffbf4e71454db0a19bfa461717c57ac863
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-29 18:46:52 -0400

    Gradient colors implemented from de useContext (no dynamical)

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import { GradientContext } from '../context/GradientContext';
 
 interface Props {
   children: JSX.Element | JSX.Element[]
 }
 
 export const GradientBackground = ({ children }: Props) => {
+
+  const { colors: { primary, secondary } } = useContext(GradientContext);
+
   return (
     <View style={{ flex: 1 }}>
       <LinearGradient
-        colors={['#2D80D8', 'white', '#00458E']}
+        colors={[primary, secondary, 'white']}
         style={{ ...StyleSheet.absoluteFillObject }}
         start={{ x: 0.1, y: 0.1 }}
         end={{ x: 0.5, y: 0.5 }}

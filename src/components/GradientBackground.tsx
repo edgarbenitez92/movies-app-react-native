516c28b4b4d565a471d7f04ab3bfb2185267316d
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-29 18:22:33 -0400

    GradientBackground component name has been fixed

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -6,7 +6,7 @@ interface Props {
   children: JSX.Element | JSX.Element[]
 }
 
-export const GrandientBackground = ({ children }: Props) => {
+export const GradientBackground = ({ children }: Props) => {
   return (
     <View style={{ flex: 1 }}>
       <LinearGradient

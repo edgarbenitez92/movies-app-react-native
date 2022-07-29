4b303ef87ec979a10ef6ec514dc04d5c41bbfff7
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-29 11:13:51 -0400

    Navigation to DetailScreen component from MoviePoster

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,5 +1,6 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Movie } from '../interfaces/movies.interfaces';
 import { moviePosterStyles } from '../styles/MoviePosterStyles';
 
@@ -11,18 +12,23 @@ interface Props {
 
 export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
 
-  let { title, poster_path } = movie;
+  let { poster_path } = movie;
 
   const uri = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
+  const navigation = useNavigation<any>();
+
   return (
-    <View style={{ width, height, ...moviePosterStyles.card }}>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('DetailScreen', movie)}
+      activeOpacity={0.95}
+      style={{ width, height, ...moviePosterStyles.card }}>
       <View style={moviePosterStyles.imageContainer}>
         <Image
           source={{ uri }}
           style={moviePosterStyles.image}
         />
       </View>
-    </View>
+    </TouchableOpacity>
   )
 }

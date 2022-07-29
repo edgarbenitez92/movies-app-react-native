4dd88c123067384e0d599f64ae5983ef6bc603a9
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-29 18:13:31 -0400

    We can get the dif colors depending on the poster movie

--- src/components/MainCarousel.tsx
+++ src/components/MainCarousel.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Carousel from 'react-native-reanimated-carousel';
+import ImageColors from 'react-native-image-colors';
 
 import { MoviePoster } from './MoviePoster';
 
 import { Movie } from '../interfaces/movies.interfaces';
 import { carouselStyles } from '../styles/CarouselStyles';
+import { getImageColors } from '../helpers/getColors';
 
 interface Props {
   movies: Movie[];
 }
 
 export const MainCarousel = ({ movies }: Props) => {
+
+  const getPosterColors = async (index: number) => {
+    const { poster_path } = movies[index];
+    const uri = `https://image.tmdb.org/t/p/w500${poster_path}`;
+
+    const [primary, secondary] = await getImageColors(uri);
+
+    console.log('colors: ', primary, secondary);
+  }
+
   return (
     <Carousel
       mode='parallax'
@@ -26,6 +38,7 @@ export const MainCarousel = ({ movies }: Props) => {
       renderItem={({ item }: any) => <MoviePoster movie={item} />}
       width={300}
       height={420}
+      onSnapToItem={(index) => getPosterColors(index)}
     />
   )
 }

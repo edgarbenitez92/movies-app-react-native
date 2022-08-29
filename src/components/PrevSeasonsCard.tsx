9652b8952f37cdef083763f2c7802a59634f373b
Edgar Benitez <edgarbenitez92@gmail.com>
2022-08-29 17:00:29 -0400

    Particular styles to prevSeasonsCard

--- src/components/PrevSeasonsCard.tsx
+++ src/components/PrevSeasonsCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { prevSeasonCardStyles } from '../styles/PrevSeasonCardStyles';
 
 interface Props {
   poster_path: string;
@@ -29,41 +30,3 @@ export const PrevSeasonsCard = ({ poster_path, season_name, season_number }: Pro
     </View>
   )
 }
-
-
-const prevSeasonCardStyles = StyleSheet.create({
-  cardContainer: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  card: {
-    marginHorizontal: 5,
-    paddingBottom: 20,
-    paddingHorizontal: 5,
-    height: 300,
-  },
-  imageContainer: {
-    flex: 1,
-    borderRadius: 18,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.32,
-    shadowRadius: 5.46,
-    elevation: 9
-  },
-  image: {
-    width: 215,
-    height: 300,
-    borderRadius: 18,
-  },
-  seasonTitle: {
-    fontWeight: 'bold',
-    marginTop: 10,
-    width: 200,
-    textAlign: 'center'
-  }
-});
